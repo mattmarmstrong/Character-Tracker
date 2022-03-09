4fbb61151599777a66f41225f9e28d26190350fb
Matthew Fleischfresser <mjf648@usask.ca>
2022-03-08 20:02:12 -0600

    testing new changes

--- proto/settings.py
+++ proto/settings.py
@@ -37,8 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
+
     'base.apps.BaseConfig',
+    'multiselectfield'
 ]
 
 MIDDLEWARE = [
