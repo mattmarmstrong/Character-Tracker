bbfc2de7bb860bf66560a93977836e856287b0f0
Senpai-Chans Yaoi PC <jesse.selin@sasktel.net>
2022-03-10 19:06:06 -0600

    Implemented the Spike Epic 1 on a branch not multiple commits behind main.
    Signed-off-by: Senpai-Chans Yaoi PC <jesse.selin@sasktel.net>

--- base/models.py
+++ base/models.py
@@ -18,6 +18,7 @@ class Room(models.Model):
 
 
 """
+model that will be implemented in next sprint, work in progress
 class Feature(models.Model):
     name = models.CharField(max_length=60, null=True)
     F_CHOICES = [
@@ -32,6 +33,7 @@ class Feature(models.Model):
         return self.name
 """
 """
+model that will be implemented in next sprint, work in progress
 class Equipment(models.Model):
     name = models.CharField(max_length=60, null=True)
     IS_WEAPON = [
@@ -47,8 +49,14 @@ class Equipment(models.Model):
 
 
 class Sheet(models.Model):
+    """
+    The sheet model that users will use CRUD operations, fields are relevant to 5e DnD playable character
+    """
+
+    # header
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     name = models.CharField(max_length=60, null=True)
+    lvl = models.IntegerField(default=1, validators=[MaxValueValidator(20), MinValueValidator(1)])
     race = models.CharField(max_length=60, null=True)
 
     CLASS_CHOICES = [
@@ -58,25 +66,44 @@ class Sheet(models.Model):
     ]
     classes = models.CharField(max_length=20, choices=CLASS_CHOICES, default='Fighter')
 
-    lvl = models.IntegerField(default=1, validators=[MaxValueValidator(20), MinValueValidator(1)])
-    exp = models.IntegerField(default=0, validators=[MinValueValidator(0)])
     background = models.CharField(max_length=60, null=True)
-    inspiration = models.IntegerField(default=10, validators=[MinValueValidator(0)])
 
-    armourAC = models.IntegerField(default=10, validators=[MinValueValidator(0)])
-    initiationRoll = models.IntegerField(default=0, validators=[MinValueValidator(0)])
-    maxHp = models.IntegerField(default=10, validators=[MinValueValidator(0)])
-    current = models.IntegerField(default=10)
-    tempHp = models.IntegerField(default=0, validators=[MinValueValidator(0)])
-    speed = models.IntegerField(default=30, validators=[MinValueValidator(0)])
+    ALIGN_CHOICES = [
+        ('Lawful Good', 'Lawful Good'),
+        ('Neutral Good', 'Neutral Good'),
+        ('Chaotic Good', 'Chaotic Good'),
+        ('Lawful Neutral', 'Lawful Neutral'),
+        ('True Neutral', 'True Neutral'),
+        ('Chaotic Neutral', 'Chaotic Neutral'),
+        ('Lawful Evil', 'Lawful Evil'),
+        ('Neutral Evil', 'Neutral Evil'),
+        ('Chaotic Evil', 'Chaotic Evil'),
+    ]
+    alignment = models.CharField(max_length=20, choices=ALIGN_CHOICES, default='Chaotic Good')
+    exp = models.IntegerField(default=0, validators=[MinValueValidator(0)])
+    inspiration = models.IntegerField(default=0, validators=[MinValueValidator(0)])
 
-    deathFail = models.IntegerField(default=0, validators=[MaxValueValidator(3), MinValueValidator(0)])
-    deathSuccess = models.IntegerField(default=0, validators=[MaxValueValidator(3), MinValueValidator(0)])
+    # statistics
+    str = models.IntegerField(default=10, validators=[MaxValueValidator(30), MinValueValidator(0)])
+    dex = models.IntegerField(default=10, validators=[MaxValueValidator(30), MinValueValidator(0)])
+    con = models.IntegerField(default=10, validators=[MaxValueValidator(30), MinValueValidator(0)])
+    wis = models.IntegerField(default=10, validators=[MaxValueValidator(30), MinValueValidator(0)])
+    int = models.IntegerField(default=10, validators=[MaxValueValidator(30), MinValueValidator(0)])
+    cha = models.IntegerField(default=10, validators=[MaxValueValidator(30), MinValueValidator(0)])
 
-    pp = models.IntegerField(default=0, validators=[MinValueValidator(0)])
-    gp = models.IntegerField(default=0, validators=[MinValueValidator(0)])
-    sp = models.IntegerField(default=0, validators=[MinValueValidator(0)])
-    cp = models.IntegerField(default=0, validators=[MinValueValidator(0)])
+    # saving throws
+    SAV_THROW = [('Str', 'Strength'),
+                 ('Dex', 'Dexterity'),
+                 ('Con', 'Constitution'),
+                 ('Int', 'Intelligence'),
+                 ('Wis', 'Wisdom'),
+                 ('Cha', 'Charisma')
+                 ]
+    savThrow = MultiSelectField(choices=SAV_THROW, blank=True)
+
+    # battle
+    numHitDice = models.IntegerField(default=0, validators=[MinValueValidator(0)])
+    currentHitDice = models.IntegerField(default=0, validators=[MinValueValidator(0)])
 
     DICE_CHOICES = [
         ('d4', 'd4'),
@@ -85,38 +112,24 @@ class Sheet(models.Model):
         ('d10', 'd10'),
         ('d12', 'd12'),
     ]
-    numHitDice = models.IntegerField(default=0, validators=[MinValueValidator(0)])
-    currentHitDice = models.IntegerField(default=0, validators=[MinValueValidator(0)]);
     hitDice = models.CharField(max_length=3, choices=DICE_CHOICES, default="d6")
+    initiationRoll = models.IntegerField(default=0, validators=[MinValueValidator(0)])
+    speed = models.IntegerField(default=30, validators=[MinValueValidator(0)])
+    maxHp = models.IntegerField(default=10, validators=[MinValueValidator(0)])
+    current = models.IntegerField(default=10)
+    tempHp = models.IntegerField(default=0, validators=[MinValueValidator(0)])
+    deathFail = models.IntegerField(default=0, validators=[MaxValueValidator(3), MinValueValidator(0)])
+    deathSuccess = models.IntegerField(default=0, validators=[MaxValueValidator(3), MinValueValidator(0)])
 
-    ALIGN_CHOICES = [
-        ('LG', 'Lawful Good'),
-        ('NG', 'Neutral Good'),
-        ('CG', 'Chaotic Good'),
-        ('LN', 'Lawful Neutral'),
-        ('TN', 'True Neutral'),
-        ('CN', 'Chaotic Neutral'),
-        ('LE', 'Lawful Evil'),
-        ('NE', 'Neutral Evil'),
-        ('CE', 'Chaotic Evil'),
-    ]
-    alignment = models.CharField(max_length=2, choices=ALIGN_CHOICES, default='CG')
+    # equipment
+    armourAC = models.IntegerField(default=10, validators=[MinValueValidator(0)])
+    pp = models.IntegerField(default=0, validators=[MinValueValidator(0)])
+    gp = models.IntegerField(default=0, validators=[MinValueValidator(0)])
+    sp = models.IntegerField(default=0, validators=[MinValueValidator(0)])
+    cp = models.IntegerField(default=0, validators=[MinValueValidator(0)])
+    equipment = models.TextField(null=True, blank=True)
 
-    str = models.IntegerField(default=10, validators=[MaxValueValidator(30), MinValueValidator(0)])
-    dex = models.IntegerField(default=10, validators=[MaxValueValidator(30), MinValueValidator(0)])
-    con = models.IntegerField(default=10, validators=[MaxValueValidator(30), MinValueValidator(0)])
-    wis = models.IntegerField(default=10, validators=[MaxValueValidator(30), MinValueValidator(0)])
-    int = models.IntegerField(default=10, validators=[MaxValueValidator(30), MinValueValidator(0)])
-    cha = models.IntegerField(default=10, validators=[MaxValueValidator(30), MinValueValidator(0)])
-
-    language = models.TextField(null=True, blank=True)
-    proficiencies = models.TextField(null=True, blank=True)
-
-    personality = models.TextField(null=True, blank=True)
-    ideals = models.TextField(null=True, blank=True)
-    bonds = models.TextField(null=True, blank=True)
-    flaws = models.TextField(null=True, blank=True)
-
+    # skills
     SKILL_CHOICES = (('Acrobatics', 'Acrobatics (Dex)'),
                      ('Animal Handling', 'Animal Handling (Wis)'),
                      ('Arcana', 'Arcana (Int)'),
@@ -127,7 +140,7 @@ class Sheet(models.Model):
                      ('Intimidation', 'Intimidation (Cha)'),
                      ('Investigation', 'Investigation (Int)'),
                      ('Medicine', 'Medicine (Wis)'),
-                     ('Nature', 'Nature (Int)'),
+                     ('Nature', 'Nature (Wis)'),
                      ('Perception', 'Perception (Wis)'),
                      ('Performance', 'Performance (Cha)'),
                      ('Persuasion', 'Persuasion (Cha)'),
@@ -136,21 +149,22 @@ class Sheet(models.Model):
                      ('Stealth', 'Stealth (Dex)'),
                      ('Survival', 'Survival (Wis)'),
                      )
-    skills = MultiSelectField(choices=SKILL_CHOICES, default='Acrobatics')
+    skills = MultiSelectField(choices=SKILL_CHOICES, blank=True)
 
-    SAV_THROW = (('Str', 'Strength'),
-                 ('Dex', 'Dexterity'),
-                 ('Con', 'Constitution'),
-                 ('Int', 'Intelligence'),
-                 ('Wis', 'Wisdom'),
-                 ('Cha', 'Charisma')
-                 )
-    savThrow = MultiSelectField(choices=SAV_THROW, blank=True, default='Str')
+    # language and other proficiencies
+    language = models.TextField(null=True, blank=True)
+    proficiencies = models.TextField(null=True, blank=True)
 
+    # personality
+    personality = models.TextField(null=True, blank=True)
+    ideals = models.TextField(null=True, blank=True)
+    bonds = models.TextField(null=True, blank=True)
+    flaws = models.TextField(null=True, blank=True)
+
+    # other
+    features = models.TextField(null=True, blank=True)
+    spells = models.TextField(null=True, blank=True)
     notes = models.TextField(null=True, blank=True)
-
-    # features = models.ManyToManyField(Feature)
-    # equipments = models.ManyToManyField(Equipment)
 
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
--- base/views.py
+++ base/views.py
@@ -1,6 +1,6 @@
 from multiprocessing import context
 
-from django.http import HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.models import User
@@ -63,75 +63,96 @@ def logoutUser(request):
     return redirect('home')
 
 
+@login_required(login_url='/login')
 def home(request):
     rooms = Room.objects.all()
     sheets = Sheet.objects.all()
     context = {'rooms': rooms, 'sheets': sheets}
     return render(request, 'base/home.html', context)
 
+
+@login_required(login_url='/login')
 def glossary(request):
     room = Room.objects.all()
     context = {'room': room}
     return render(request, 'base/glossary.html', context)
 
 
+@login_required(login_url='/login')
 def room(request, pk):
     room = Room.objects.get(id=pk)
     context = {'room': room}
     return render(request, 'base/room.html', context)
 
 
+@login_required(login_url='/login')
 def sheet(request, pk):
     sheet = Sheet.objects.get(id=pk)
+
+    if request.user != sheet.user and not request.user.is_superuser:  # only admins or users can view their own sheets
+        return redirect('home')
+
     context = {'sheet': sheet}
     return render(request, 'base/sheet.html', context)
 
 
-def createSheet(request):
+@login_required(login_url='/login')  # can only be used by users that are logged in, else redirect to login page
+def create_sheet(request):
     """
     Create sheet and save if valid.
     :param request: object to pass state to the function
-    :return: url to return to
+    :return: url to redirect the user to
     """
     form = SheetForm()  # get default form
+
     if request.method == 'POST':
         form = SheetForm(request.POST)  # get form for post
         if form.is_valid():  # if valid, save, then redirect
+            form = form.save(commit=False)  # save the form, then update user to user who created the sheet
+            form.user = request.user
             form.save()
-            return HttpResponseRedirect('home')
+            return redirect('home')
 
     context = {'form': form}
     return render(request, 'base/sheet_form.html', context)
 
 
-def updateSheet(request, pk):
+@login_required(login_url='/login')  # can only be used by users that are logged in, else redirect to login page
+def update_sheet(request, pk):
     """
     Edit the sheet and saves the data if valid.
     :param request: object to pass state to the function
     :param pk: ID for the sheet
-    :return: url to return to
+    :return: url to redirect the user to
     """
     sheet = Sheet.objects.get(id=pk)  # get sheet with ID
     form = SheetForm(instance=sheet)  # get current form
 
+    if request.user != sheet.user and not request.user.is_superuser:  # only admins or users can edit their own sheets
+        return redirect('home')
+
     if request.method == 'POST':
-        origin = request.POST.get('origin', '/')  # get prev location to redirect
         form = SheetForm(request.POST, instance=sheet)  # get form for post
         if form.is_valid():  # if valid, save, then redirect to previous location
             form.save()
             return redirect(f'/sheet/{pk}')
 
     context = {'form': form}
-    return render(request, 'base/sheet_form.html', context)  # otherwise don't leave
+    return render(request, 'base/sheet_form.html', context)  # render existing form
 
 
-def deleteSheet(request, pk):
+@login_required(login_url='/login')  # can only be used by users that are logged in, else redirect to login page
+def delete_sheet(request, pk):
     """
     Delete sheet on user confirmation.
     :param request: object to pass state to the function
-    :return: url to return to
+    :return: url to redirect the user to
     """
     sheet = Sheet.objects.get(id=pk)
+
+    if request.user != sheet.user and not request.user.is_superuser:  # only admins or users can delete their own sheets
+        return redirect('home')
+
     if request.method == 'POST':
         sheet.delete()
         return redirect('home')
